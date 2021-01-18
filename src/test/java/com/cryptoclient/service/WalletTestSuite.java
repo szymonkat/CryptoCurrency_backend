4e00a7dc797d21536972b17a7dc29a1d1971b4db
szymonkat <szymonkat@gmail.com>
2021-01-18 20:05:01 +0100

    Wallet_service_start

--- src/test/java/com/cryptoclient/service/WalletTestSuite.java
+++ src/test/java/com/cryptoclient/service/WalletTestSuite.java
@@ -1,10 +1,15 @@
 package com.cryptoclient.service;
 
+import com.cryptoclient.domain.Wallet;
+import com.cryptoclient.repository.WalletRepository;
 import com.cryptoclient.service.interfaces.WalletService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -13,4 +18,25 @@ public class WalletTestSuite {
     @Autowired
     private WalletService walletService;
 
+    @Autowired
+    private WalletRepository walletRepository;
+
+    private Wallet wallet = new Wallet("TestWallet");
+
+    @Test
+    public void shouldGetAllWallets() {
+        //Given
+        
+        //When
+        Wallet createdWallet = walletService.createWallet(wallet);
+        //Then
+        assertNotEquals(createdWallet.getId(), null);
+        //Cleanup
+        walletRepository.deleteById(createdWallet.getId());
+    }
 }
+
+//Given
+//When
+//Then
+//Cleanup
