51614953990742996595bee8731a70db3b1d32b9
szymonkat <szymonkat@gmail.com>
2021-01-26 16:43:08 +0100

    Update WalletItemTestSuite.java

--- src/test/java/com/cryptoclient/service/WalletItemTestSuite.java
+++ src/test/java/com/cryptoclient/service/WalletItemTestSuite.java
@@ -1,10 +1,12 @@
 package com.cryptoclient.service;
 
 import com.cryptoclient.domain.Currency;
+import com.cryptoclient.domain.Wallet;
 import com.cryptoclient.domain.WalletItem;
 import com.cryptoclient.exceptions.NotFoundException;
 import com.cryptoclient.repository.WalletItemRepository;
 import com.cryptoclient.service.interfaces.WalletItemService;
+import com.cryptoclient.service.interfaces.WalletService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,16 +24,30 @@ public class WalletItemTestSuite {
     private WalletItemService walletItemService;
 
     @Autowired
+    private WalletService walletService;
+
+    @Autowired
     private WalletItemRepository walletItemRepository;
 
     LocalDateTime now = LocalDateTime.now();
     String various = now.toString();
+    Wallet wallet = new Wallet(various + "1");
+
 
     @Test
     public void shouldModifyWallet() {
         //Given
+        walletService.createWallet(wallet);
+        WalletItem walletItem = new WalletItem(wallet, Currency.USD, 300.0);
+        walletItemService.save(walletItem);
+        Long walletItemLong = walletItem.getId();
+
+        WalletItem newWalletItem = new WalletItem(wallet, Currency.BTC, 200.0);
+
         //When
+        walletItemService.modifyWalletItem(newWalletItem);
         //Then
+        // DO POPRAWKI ALBO USUNIECIA JEZELI NIKT NIE KORZYSTA
         //CleanUp
     }
 
