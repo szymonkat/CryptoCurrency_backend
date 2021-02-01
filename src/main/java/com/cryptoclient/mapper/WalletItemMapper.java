4a0b46be62ffa595dda491e4a0491c9fd5885d41
szymonkat <szymonkat@gmail.com>
2021-02-01 13:01:28 +0100

    WalletItemTestMapperFinished

--- src/main/java/com/cryptoclient/mapper/WalletItemMapper.java
+++ src/main/java/com/cryptoclient/mapper/WalletItemMapper.java
@@ -6,7 +6,6 @@ import com.cryptoclient.service.interfaces.WalletService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
--- src/test/java/com/cryptoclient/mapper/WalletMapperTestSuite.java
+++ src/test/java/com/cryptoclient/mapper/WalletMapperTestSuite.java
@@ -14,7 +14,6 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
@@ -27,7 +26,7 @@ public class WalletMapperTestSuite {
     @InjectMocks
     private WalletMapper walletMapper;
 
-    @Mock
+    @InjectMocks
     private WalletItemMapper walletItemMapper;
 
     @Mock
@@ -83,6 +82,8 @@ public class WalletMapperTestSuite {
 
         System.out.println(walletDto1.getWalletItemList().toString());
         System.out.println(walletList);
+
+
         //Then
         Assert.assertEquals(walletList.get(0).getId(), Long.valueOf(5));
         Assert.assertEquals(walletList.get(0).getName(), "wallet1");
@@ -113,14 +114,18 @@ public class WalletMapperTestSuite {
 
     @Test
     public void testIfWalletItemMapperWorks() {
-/*        WalletItemDto walletItemDto1 = new WalletItemDto(13L, 5L, Currency.BTC, 300.0);
+        WalletItemDto walletItemDto1 = new WalletItemDto(13L, 5L, Currency.BTC, 300.0);
         WalletItemDto walletItemDto2 = new WalletItemDto(14L, 6L, Currency.USD, 300.0);
 
         List<WalletItemDto> walletItemDtoList = new ArrayList<>();
         walletItemDtoList.add(walletItemDto1);
         walletItemDtoList.add(walletItemDto2);
 
-        walletItemMapper.*/
+        List<WalletItem> walletItemList = walletItemMapper.mapToWalletItemList(walletItemDtoList);
+
+        System.out.println("TESTING");
+        System.out.println(walletItemList);
+        System.out.println("TESTING_ENDS");
     }
 
 }
